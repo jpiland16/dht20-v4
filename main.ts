66112429380310454894d2f59d15dbc49e8e33bf
Jonathan Piland <jpiland16@gmail.com>
2025-11-07 09:04:35 -0500

    simplify demo code

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 DHT20.initializeDht(DigitalPin.P2, DigitalPin.P1)
 basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        . # . # .
+        . . # . .
+        . # . # .
+        # # # # #
+        `)
     DHT20.readDht()
-    basic.showString("" + (DHT20.getTemp()))
-    basic.pause(1000)
+    basic.clearScreen()
+    basic.showString("" + Math.round(DHT20.getTemp()) + "C")
+    basic.pause(200)
+    basic.showString("" + Math.round(DHT20.getHumidity()) + "%")
+    basic.pause(200)
 })
